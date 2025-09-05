86cc5d1dea928230730cfa4982f0fd899fb0c197
Angelo Rios <angelorios@live.com>
2025-09-05 20:39:46 +0200

    ref: check malloc and remove temp var

--- list_utils.c
+++ list_utils.c
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 19:17:48 by angrios           #+#    #+#             */
-/*   Updated: 2025/09/05 19:52:26 by angrios          ###   ########.fr       */
+/*   Updated: 2025/09/05 20:37:00 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,24 @@ t_node	*create_node(int value)
 {
 	t_node	*new_node;
 
-	new_node = NULL;
 	new_node = malloc(sizeof(t_node));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->node_data.stack_value = value;
 	new_node->next_node = NULL;
 	return (new_node);
 }
 
 // Put node at the begining of the list
-void	push_node(t_node **stack, int value)
+void	push_node(t_node **stack, t_node *new_node)
 {
-	
+	if (new_node == NULL)
+		return (NULL);
+	if (*stack == NULL)
+		*stack = new_node;
+	else
+	{
+		new_node->next_node = *stack;
+		*stack = new_node;
+	}
 }
