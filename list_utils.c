508a5e222fa0d89e70414aa4624da61776824cca
Angelo Rios <angelorios@live.com>
2025-10-28 17:25:54 +0100

    add: fn pop_node & peek_node

--- list_utils.c
+++ list_utils.c
@@ -6,13 +6,12 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 19:17:48 by angrios           #+#    #+#             */
-/*   Updated: 2025/09/05 20:37:00 by angrios          ###   ########.fr       */
+/*   Updated: 2025/10/28 17:22:17 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// Create a node
 t_node	*create_node(int value)
 {
 	t_node	*new_node;
@@ -25,7 +24,6 @@ t_node	*create_node(int value)
 	return (new_node);
 }
 
-// Put node at the begining of the list
 void	push_node(t_node **stack, t_node *new_node)
 {
 	if (new_node == NULL)
@@ -37,4 +35,25 @@ void	push_node(t_node **stack, t_node *new_node)
 		new_node->next_node = *stack;
 		*stack = new_node;
 	}
+}
+
+int	pop_node(t_node **stack, int *out_value)
+{
+	t_node	*temp;
+
+	if (stack == NULL || *stack == NULL || out_value == NULL)
+		return (0);
+	temp = *stack;
+	*out_value = temp->node_data.stack_value;
+	*stack = temp->next_node;
+	free(temp);
+	return (1);
+}
+
+int	peek_node(t_node *stack, int *out_value)
+{
+	if (stack == NULL || out_value == NULL)
+		return (0);
+	*out_value = stack->node_data.stack_value;
+	return (1);
 }
