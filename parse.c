87389b5793a04054afe1b6bc22098f56c4b946b3
Angelo Rios <angelorios@live.com>
2025-12-01 16:59:49 +0100

    add: parser, wip

---
+++ parse.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 16:24:46 by angrios           #+#    #+#             */
+/*   Updated: 2025/12/01 16:58:32 by angrios          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	is_number(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || str[0]== '\0')
+		return (0);
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static long	ft_atol(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (str[0] == '-' || str[0] == '+')
+	{
+		if (str[0] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+static int	has_duplicates(t_node *stack)
+{
+	t_node	*current;
+	t_node	*runner;
+
+	current = stack;
+	while (current)
+	{
+		runner = current->next;
+		while (runner)
+		{
+			if (runner->value == current->value)
+				return (1);
+			runner = runner->next;
+		}
+		current = current->next;
+	}
+	return (0);
+}
+
+int	parse_args(int argc, char **argv, t_node **stack_a)
+{
+
+}
